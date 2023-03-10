1804331e61acbe67897b5e9d3a5fbd262cd3b76d
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-10 15:54:27 -0300

    feat: Create and List routes (HTTP methods)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
 import http from 'node:http'
 
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuários')
+  }
+
   return res.end('hello world')
 })
 
-server.listen('3333') //localhost:3333
+server.listen('3333')
