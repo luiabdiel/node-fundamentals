a87df8d6d165b677c6fdecc3588eecdf6a6669fd
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-10 19:50:43 -0300

    feat: Creating a reading stream

---
+++ streams/fundamentals.js
@@ -0,0 +1,26 @@
+// STREAMS ->
+
+import { Readable } from 'node:stream'
+import { setTimeout } from 'node:timers/promises'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } 
+      else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
