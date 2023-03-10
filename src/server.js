b018c64b35327731509a0b0eb27bb713594d2b2a
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-10 16:38:36 -0300

    feat: Saving users in memory (Headers)

--- src/server.js
+++ src/server.js
@@ -1,28 +1,23 @@
 import http from 'node:http'
 
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
+const users = []
 
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Cristiano',
+      email: 'cristiano@gmail.com'
+    })
+
     return res.end('Criação de usuários')
   }
 
